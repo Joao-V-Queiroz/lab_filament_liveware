3f46c8c5799c6b2b7e4310a8ddb787943da44cbd
inkeyitservice <inkeyitservice@gmail.com>
2023-08-22 11:59:33 -0300

    :sparkles: criação do primeiro panel e instalação do Filament

---
+++ database/migrations/2023_08_22_145138_insert_users_admin.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         DB::table('users')->insert([
+            'name' => 'João Victor Queiroz',
+            'email' => 'joao.teste@gmail.com',
+            'password' => bcrypt('abcd12345'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
